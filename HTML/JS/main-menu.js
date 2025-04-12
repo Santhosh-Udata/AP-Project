62cc1c0f4bd85e56926a8ed13d910264e85b3a99
Santhosh Udata <santhoshudata07@gmail.com>
2025-04-13 00:35:26 +0530

    clockingout

---
+++ HTML/JS/main-menu.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    const param = document.body.dataset.page;
+    document.getElementsByClassName(param)[0].style.backgroundColor = "rgb(227, 255, 226)";
+    document.getElementsByClassName(param)[1].style.color = "black";
+};
