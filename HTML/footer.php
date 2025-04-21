d5a626491bc7fd33e04bd41b3ca3ec848e9cdb24
Santhosh Udata <santhoshudata07@gmail.com>
2025-04-21 13:12:55 +0530

    completed except data

---
+++ HTML/footer.php
@@ -0,0 +1,34 @@
+<!-- footer.php -->
+<link rel="stylesheet" href="CSS/footer.css">
+<footer class="footer">
+  <div class="foot-container">
+    <div class="row">
+      <div class="footer-col">
+        <h4>Contact Us:</h4>
+        <ul class="icon-list">
+          <li><span class="material-icons">email</span>gymlover@gmail.com</li>
+          <li><span class="material-icons">phone</span>140 254 1234</li>
+          <li><span class="material-icons">place</span>Andhra Pradesh</li>
+        </ul>
+      </div>
+      <div class="footer-col">
+        <h4>Social Media:</h4>
+        <div class="social-media">
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-facebook"></i></a>
+        </div>
+      </div>
+      <div class="footer-col">
+        <h4>Quick Links:</h4>
+        <ul class="icon-list-1">
+          <li><a href="gallery.php">Gallery</a></li>
+          <li><a href="partners.html">Our Partners</a></li>
+          <li><a href="aboutus.html">About Us</a></li>
+          <li><a href="#">Guidelines</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
