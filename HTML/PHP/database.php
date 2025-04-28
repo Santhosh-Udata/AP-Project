74d79b21dc751edbb6d79b5af5734ca9e864fe30
Santhosh Udata <santhoshudata07@gmail.com>
2025-04-28 14:16:29 +0530

    save

--- HTML/PHP/database.php
+++ HTML/PHP/database.php
@@ -9,10 +9,8 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Connection successful message (optional)
     // echo "Connected successfully";
 } catch (PDOException $e) {
-    // Handle connection error
     echo "Connection failed: " . $e->getMessage();
     exit;
 }
